024f63e973d9365cf9d4492fc858e7d2dac9b1e3
funkybeppe <lucabarbero1996@gmail.com>
2022-09-21 10:48:05 +0000

    Add main game functions and ascii art

--- run.py
+++ run.py
@@ -1,3 +1,62 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def roll_dice():
+
+    dice_drawing = {
+        1: (
+            "-----",
+            "|   |",
+            "| o |",
+            "|   |",
+            "-----",
+        ),
+        2: (
+            "-----",
+            "|o  |",
+            "|   |",
+            "|  o|",
+            "-----",
+        ),
+        3: (
+            "-----",
+            "|o  |",
+            "| o |",
+            "|  o|",
+            "-----",
+        ),
+        4: (
+            "-----",
+            "|o o|",
+            "|   |",
+            "|o o|",
+            "-----",
+        ),
+        5: (
+            "-----"
+            "|o o|"
+            "| o |"
+            "|o o|"
+            "-----"
+        ),
+        6: (
+            "-----",
+            "|o o|",
+            "|o o|",
+            "|o o|",
+            "-----",
+        ),
+    }
+    roll = input("Roll dice ? (Yes/No): ")
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        print(f"dice rolled: {dice1} and {dice2}")
+        print("\n".join(dice_drawing[dice1]))
+        print("\n".join(dice_drawing[dice2]))
+
+        roll = input("Roll again? (Yes/No): ")
+
+
+roll_dice()
